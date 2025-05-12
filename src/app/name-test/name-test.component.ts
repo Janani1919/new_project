7428fa25e4ea9ec1376c4a49510c8af083a499d3
Janani1919 <jananirajendran1996@gmail.com>
2025-05-12 13:28:25 +0530

    testing (#1)

---
+++ src/app/name-test/name-test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-name-test',
+  imports: [],
+  templateUrl: './name-test.component.html',
+  styleUrl: './name-test.component.css'
+})
+export class NameTestComponent {
+
+}
